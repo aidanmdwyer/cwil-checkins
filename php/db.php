adb12fb4b66c3987d7f978ecf37d50d561683fa0
adwyer02 <aidandyr@gmail.com>
2026-05-12 21:47:12 -0500

    v1.1 db user

--- php/db.php
+++ php/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "cwiltoo1_checkins", "Cwil2622!", "cwiltoo1_checkins");
+$conn = new mysqli("localhost", "cwiltoo1_cwil", "Cwil2622!", "cwiltoo1_checkins");
 
 if ($conn->connect_errno) {
     die('Database connection failed: ' . $conn->connect_error);
