00807c0bbdee4bc05b8d8ed0ffaa4fd1ef22b3d6
adwyer02 <aidandyr@gmail.com>
2026-06-18 19:57:49 -0500

    v1.3 dynamic instructions page

--- php/instructions.php
+++ php/instructions.php
@@ -2,7 +2,6 @@
 include 'login.php';
 include 'accountProperties.php';
 
-$accountProperties = $_SESSION['accountProperties'];
 $accountType = $_SESSION['accountType'];
 ?>
 
@@ -21,6 +20,12 @@ $accountType = $_SESSION['accountType'];
     <h1>Check-in App Instructions</h1>
     <p>Please refer to these instructions on how to use the City Wide Check-ins App as an <?php echo $accountType ?>.</p>
 </header>
+<main>
+    <?php if(accountProperties("Home Page")) { ?>
+        <h2>Home Page</h2>
+        <p>Home Page!!!</p>
+    <?php } ?>
+</main>
 </body>
 
 <style>
@@ -30,11 +35,40 @@ $accountType = $_SESSION['accountType'];
         gap: 15px;
         align-items: center;
     }
+    header h1 {
+        margin: 0;
+        padding: 0;
+    }
+    header p {
+        font-size: 1.5rem;
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        align-items: flex-start;
+    }
     h1 {
-        font-family: Tahoma, sans-serif
+        font-family: Tahoma, sans-serif;
+        margin: 1rem 0 2rem 0;
+        padding: 0;
+    }
+    h2 {
+        font-family: Tahoma, sans-serif;
+        margin: 1rem 0 2rem 0;
+        padding: 0;
+    }
+    h3 {
+        font-family: Tahoma, sans-serif;
+        margin: 1rem 0 2rem 0;
+        padding: 0;
     }
     p {
-        font-family: "Times New Roman", times, serif
+        font-family: "Times New Roman", times, serif;
+        font-size: 1.2rem;
+        margin: 0;
+        padding: 0;
     }
 </style>
 </html>
