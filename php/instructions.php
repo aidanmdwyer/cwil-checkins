9361ccc8466e64157aea69ef81714738b3a21f95
adwyer02 <aidandyr@gmail.com>
2026-06-18 22:23:01 -0500

    v1.3 dynamic instructions page

--- php/instructions.php
+++ php/instructions.php
@@ -31,8 +31,8 @@ $accountType = $_SESSION['accountType'];
                 <h2>Home Page (Viewing Buildings)</h2>
                 <p>
                     Once logged in, you will see a table of buildings. Here, you can see the building name, which City Wide
-                    manager is responsible for it, the IC assigned to it, the days it gets cleaned, whether the crew has arrived
-                    today, and the time they checked in.
+                    manager is responsible for it, the IC assigned to it, the days it gets cleaned, and whether the crew has arrived
+                    today
                     <br><br>By default, the “Today Only” filter will be turned on, showing only buildings that are scheduled to get
                     cleaned on the current day. You can uncheck this to see all buildings. You can also use the manager and
                     IC filters to view only buildings that are assigned to a specific manager or contractor.
@@ -110,7 +110,7 @@ $accountType = $_SESSION['accountType'];
         align-items: center;
         text-align: center;
     }
-    #index h1 {
+    #index h2 {
         margin: 0;
         padding: 0;
     }
