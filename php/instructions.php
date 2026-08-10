c0b789eb0685a3862bdbb757ed2bc48dbbb565aa
adwyer02 <aidandyr@gmail.com>
2026-08-09 21:47:21 -0500

    v1.3 dynamic instructions page

--- php/instructions.php
+++ php/instructions.php
@@ -336,11 +336,9 @@ function implodeCommas(array $items, string $finalSeparator): string {
         #index {
             display: none;
         }
-        img, div, main {
-            break-inside: avoid;
-        }
         img {
-            display: table;
+            page-break-inside: avoid;
+            break-inside: avoid;
         }
     }
     #page {
