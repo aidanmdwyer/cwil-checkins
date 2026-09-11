3dfc0ed1c9fffa2a5806b1a5ea43af07577501eb
adwyer02 <aidandyr@gmail.com>
2026-09-10 20:22:30 -0500

    v1.3.1 added check in statistics to main buildings page

--- js/adjustMainMargin.js
+++ js/adjustMainMargin.js
@@ -7,7 +7,7 @@ function adjustMainMargin() {
     if(tableMenu) {
         tableMenu.style.display = 'flex';
         tableMenu.style.top = header.offsetHeight + "px";
-        mainMargin += tableMenu.offsetHeight + 10;
+        mainMargin += tableMenu.offsetHeight;
     } else {
         mainMargin += 30;
     }
