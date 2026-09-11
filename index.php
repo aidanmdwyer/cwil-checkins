44bc66f52a9e08a4d62537a9ba994d2fbdbd7894
adwyer02 <aidandyr@gmail.com>
2026-09-10 20:23:32 -0500

    v1.3.1 added check in statistics to main buildings page

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ if (!accountProperties('Home Page')) {
         </div>
     </div>
 
-    <div id="checkInCounter"></div>
+    <div id="checkInCounter" style="margin-top: 5px;"></div>
 
     <div style="display: flex; align-items: flex-start;">
         <!--        Table-->
