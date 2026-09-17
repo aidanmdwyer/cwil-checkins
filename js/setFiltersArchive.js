7c0cdb3d61b2c450765494a5abcb2331a86c6774
adwyer02 <aidandyr@gmail.com>
2026-09-17 18:01:30 -0500

    v1.3.2 added archive range

--- js/setFiltersArchive.js
+++ js/setFiltersArchive.js
@@ -1,9 +1,9 @@
-document.getElementById("singleDateButton").onclick(function() {
+document.getElementById("singleDateButton").addEventListener('input', function() {
     document.getElementById("archiveFromDate").style.display = "default";
     document.getElementById("archiveToDate").style.display = "default";
     document.getElementById("archiveSingleDate").style.display = "none";
 });
-document.getElementById("dateRangeButton").onclick(function() {
+document.getElementById("dateRangeButton").addEventListener('input', function() {
     document.getElementById("archiveFromDate").style.display = "none";
     document.getElementById("archiveToDate").style.display = "none";
     document.getElementById("archiveSingleDate").style.display = "default";
