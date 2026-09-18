3dd7d0c98c4a8daa405d22525f7b0ed76b04f86a
adwyer02 <aidandyr@gmail.com>
2026-09-17 19:29:32 -0500

    v1.3.2 added archive range

--- php/checkInArchives.php
+++ php/checkInArchives.php
@@ -282,8 +282,8 @@ if (!accountProperties('Archives Page')) {
                 const percentageUnchecked = Math.round((100 - percentageChecked)*100)/100;
                 let checkInCounterHTML = 
                     "<span style='margin-right: 30px;'>" + numBuildings + " buildings loaded</span>" + 
-                    "<span style='margin-right: 30px;'>" + numChecked + "/" + numBuildings + " &#9989 (" + percentageUnchecked + "%)</span>" + 
-                    (numBuildings - numChecked) + "/" + numBuildings + " &#10060 (" + (100 - percentageChecked) + "%)";
+                    "<span style='margin-right: 30px;'>" + numChecked + "/" + numBuildings + " &#9989 (" + percentageChecked + "%)</span>" + 
+                    (numBuildings - numChecked) + "/" + numBuildings + " &#10060 (" + percentageUnchecked + "%)";
                 document.getElementById("checkInCounter").innerHTML = checkInCounterHTML;
 
                 htmlStr += `</tbody>`;
